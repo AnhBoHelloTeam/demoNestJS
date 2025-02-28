3511334e75eac8f66b25da15973f541e228ca2cd
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-02-28 15:05:54 +0700

    commit coworking booking demo

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { BookingModule } from './booking/booking.module';
+import { VenueModule } from './venue/venue.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/coworking', {
+      connectionFactory: (connection) => {
+        console.log('✅ Connected to MongoDB:', connection.name);
+        return connection;
+      },
+    }),
+    UserModule,
+    BookingModule,
+    VenueModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.module.ts
@@ -0,0 +1,19 @@
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserSchema } from './user.schema';
+import { AuthMiddleware } from '../auth/auth.middleware';
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.ALL });
+  }
+}
